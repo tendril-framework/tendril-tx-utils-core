ef1fc91479d213fa2a919ebfd3421c5b1d359535
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2021-12-08 20:44:30 +0530

    Add Async Engine template

---
+++ src/tendril/asynchronous/utils/engines.py
@@ -0,0 +1,25 @@
+
+
+from tendril.asynchronous.utils.logger import TwistedLoggerMixin
+
+
+class AsyncEngineBase(TwistedLoggerMixin):
+    def __init__(self, *args, **kwargs):
+        self._running = False
+        super(AsyncEngineBase, self).__init__(*args, **kwargs)
+
+    def _start(self):
+        raise NotImplementedError
+
+    def start(self):
+        if not self._running:
+            self._start()
+            self._running = True
+
+    def _stop(self):
+        raise NotImplementedError
+
+    def stop(self):
+        if self._running:
+            self._stop()
+            self._running = False
